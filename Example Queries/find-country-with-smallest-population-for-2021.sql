1428c3d41106aafdbb47cc2854036953f5407ca9
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-12-15 10:31:20 +0200

    Delete Example Queries/find-country-with-smallest-population-for-2021.sql

--- Example Queries/find-country-with-smallest-population-for-2021.sql
+++
@@ -1,4 +0,0 @@
-SELECT CountriesByYear.[Population] AS TotalPopulation, Countries.[Name]
-FROM CountriesByYear
-JOIN Countries ON CountriesByYear.CountryId = Countries.Id
-WHERE YearId = 2021 AND CountriesByYear.[Population] = (SELECT MIN([CountriesByYear.[Population] FROM CountriesByYear)
