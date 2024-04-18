5ef2a476eb6fff70d5eb4c56cb79d9f758680bec
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2024-04-18 21:40:10 +0100

    Update find-top-5-percent-of-countries-by-population-in-2000.sql

--- Example Queries/find-top-5-percent-of-countries-by-population-in-2000.sql
+++ Example Queries/find-top-5-percent-of-countries-by-population-in-2000.sql
@@ -1,4 +1,4 @@
-SELECT Top 5 PERCENT CountriesByYear.[Population] AS [Current Population], Countries.[Name] AS Country, YearId AS [Year]
+SELECT Top 5 PERCENT CountriesByYear.[Population], Countries.[Name] AS Country
 FROM CountriesByYear
 JOIN Countries ON CountriesByYear.CountryId = Countries.Id
 WHERE YearId = '2000'
