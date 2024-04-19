2dd26e45f0859527eba228565e9880ce2856f3f1
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2024-04-19 11:40:21 +0100

    Add files via upload

---
+++ Example Queries/find-fertility-rate-for-each-north-american-country-in-1960.sql
@@ -0,0 +1,6 @@
+SELECT  Countries.[Name] AS Country, CountriesByYear.[TFR] AS TotalFertilityRate
+FROM Continents
+JOIN Countries ON Continents.Id = Countries.ContinentId
+JOIN CountriesByYear ON Countries.Id = CountriesByYear.CountryId
+WHERE ContinentId = '2' AND YearId = 1960 AND TFR IS NOT NULL
+ORDER BY TFR DESC
