22074fdd81e836aa9b790c821a24334d07f34dde
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2024-04-26 23:21:58 +0300

    Update find-all-countries-beginning-with-south-in-asia.sql

--- Example Queries/find-all-countries-beginning-with-south-in-asia.sql
+++ Example Queries/find-all-countries-beginning-with-south-in-asia.sql
@@ -1,4 +1,4 @@
-SELECT * 
+SELECT Countries.Id, Countries.[Name] AS [Country], Continents.[Name] AS [Continent]
 FROM Countries
 JOIN Continents ON Countries.ContinentId = Continents.Id
 WHERE Countries.[Name] LIKE 'South%' AND ContinentId = 4
