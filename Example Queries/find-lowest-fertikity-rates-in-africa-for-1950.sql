7cca6891ffd4063132fdf85f42e010e0bd8c7b0e
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2024-05-01 22:45:52 +0300

    Delete Example Queries/find-lowest-fertikity-rates-in-africa-for-1950.sql

--- Example Queries/find-lowest-fertikity-rates-in-africa-for-1950.sql
+++
@@ -1,6 +0,0 @@
-SELECT Top 10 c.[Name], cby.TFR
-FROM CountriesByYear AS cby
-JOIN Countries AS c on cby.CountryId = c.Id
-JOIN Continents AS ct on c.ContinentId = ct.Id
-WHERE ct.[Name] = 'Africa' AND YearId = 1950
-ORDER BY [TFR] ASC;
