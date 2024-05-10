49e3f2f062da834641f312d14617c59634dcee97
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2024-05-10 21:29:47 +0300

    Add files via upload

---
+++ Example Queries/find-years-where-china-had-population-less-than-one-billion.sql
@@ -0,0 +1,6 @@
+SELECT c.[Name], cby.TFR, cby.[Population], cby.YearId
+FROM CountriesByYear AS cby
+JOIN Countries AS c ON cby.CountryId = c.Id
+JOIN Continents AS ct ON c.ContinentId = ct.Id
+WHERE c.[Name] = 'China' AND cby.[Population] < 1000000000
+ORDER BY cby.YearId
