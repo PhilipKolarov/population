cdf166dbd832600d7dfe6c509e287c57ed2317b0
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2024-05-18 23:15:35 +0300

    Delete find-all-records-of-north-america-with-30-highest-fertility-rates.sql

--- find-all-records-of-north-america-with-30-highest-fertility-rates.sql
+++
@@ -1,6 +0,0 @@
-SELECT Top 30 Countries.[Name] AS Country, CountriesByYear.TFR, CountriesByYear.YearId
-FROM CountriesByYear
-JOIN Countries ON CountriesByYear.CountryId = Countries.Id
-JOIN Continents ON Countries.ContinentId = Continents.Id
-WHERE Continents.[Name] = 'North America'
-ORDER BY CountriesByYear.[TFR] DESC
