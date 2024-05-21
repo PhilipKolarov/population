ee832af3208ec461fb6fbc53f4be51d3e8deb972
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2024-05-21 13:06:24 +0300

    Delete find-top-10-european-countries-by-population-in-2021.sql

--- find-top-10-european-countries-by-population-in-2021.sql
+++
@@ -1,6 +0,0 @@
-SELECT Top 10 c.[Name], cby.[Population]
-FROM CountriesByYear AS cby
-JOIN Countries AS c on cby.CountryId = c.Id
-JOIN Continents AS ct on c.ContinentId = ct.Id
-WHERE ct.[Name] = 'Europe' AND YearId = 2021
-ORDER BY [Population] DESC;
